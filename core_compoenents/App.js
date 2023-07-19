e65b7d87df73a043bb2463494890ac6e0071ee1f
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2023-07-19 16:01:38 +0530

    feat: Image Background tag

--- core_compoenents/App.js
+++ core_compoenents/App.js
@@ -1,17 +1,26 @@
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, ImageBackground } from 'react-native';
 import Icon from './assets/adaptive-icon.png';
 
 export default function App() {
 	return (
 		<View style={{ flex: 1, backgroundColor: 'plum', padding: 60 }}>
-			<Text>
+			{/* <Text>
 				<Text style={{ color: 'white' }}>Hello</Text> world
-			</Text>
-			<Image style={{ width: 300, height: 300 }} source={Icon} />
+			</Text> */}
+			{/* <Image style={{ width: 300, height: 300 }} source={Icon} />
 			<Image
 				style={{ width: 300, height: 300 }}
 				source={{ uri: 'https://picsum.photos/200/300' }}
-			/>
+			/> */}
+
+			<ImageBackground
+				source={Icon}
+				style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+			>
+				<Text style={{ fontSize: 20, color: 'black', fontWeight: 600 }}>
+					Hello world
+				</Text>
+			</ImageBackground>
 		</View>
 	);
 }
